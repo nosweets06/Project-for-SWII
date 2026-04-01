02e577e47d1092ff19742d57e32d1d8915df9ae6
nosweets06 <isaollion@gmail.com>
2026-03-31 22:44:25 -0400

    Update MusicPlaylist1L.java

--- src/components/musicPlaylist/MusicPlaylist1L.java
+++ src/components/musicPlaylist/MusicPlaylist1L.java
@@ -1,8 +1,6 @@
 package components.musicPlaylist;
 //pretending to use map with osu components.
 
-import components.map.Map;
-import components.map.Map1L;
 import components.sequence.Sequence;
 import components.sequence.Sequence1L;
 
@@ -20,13 +18,13 @@ public class MusicPlaylist1L extends MusicPlaylistSecondary
     /**
      * Representation of {@code this}.
      */
-    private Map<String, Song> rep;
+    private Sequence<Song> rep;
 
     /**
      * Creator of intial representation.
      */
     private void createNewRep() {
-        this.rep = new Map1L<>();
+        this.rep = new Sequence1L<>();
     }
 
     /**
@@ -42,103 +40,106 @@ public class MusicPlaylist1L extends MusicPlaylistSecondary
 
     @Override
     public final void addSong(Song s) {
-        this.rep.add(s.getName(), s);
+        this.rep.add(this.rep.length(), s); //fixed
     }
 
+    //unsure if it should still return a song or string
     @Override
     public final Song removeSong(int s) {
-        Map.Pair<String, Song> p = this.rep.remove(s);
+        Song p = this.rep.remove(s);
 
-        return p.value();
-    }
-
-    @Override
-    public final Song removeSong() {
-        Map.Pair<String, Song> p = this.rep.removeAny();
-        return p.value();
-    }
-
-    @Override
-    public final Song getSong(String s) {
-        Song p = this.rep.value(s);
         return p;
     }
 
     @Override
-    public final void playSong(String s) {
-        Song p = this.rep.value(s);
-        System.out.println("Name :" + p.getName());
-        System.out.println("Duration :" + p.getDuration());
+    public final Song removeLastSong() {
+        Song p = this.rep.remove(this.rep.length() - 1);
+        return p;
     }
+
+    @Override
+    public final Song getSong(int n) { //change so gives position in seq
+        Song p = this.rep.entry(n);
+        return p;
+    }
+
+    //no longer in kernel
+
+    // @Override
+    // public final void shuffle() {
+    //     assert this.rep.length() > 1 : "Must be length of greater than 1.";
+
+    //     Sequence<Map.Pair<String, Song>> temp = new Sequence1L<>();
+
+    //     for (Map.Pair<String, Song> p : this.rep) {
+    //         temp.add(temp.length(), p);
+    //     }
+
+    //     //randomizer here here unsure of how to implement
+
+    // }
+
+    // @Override
+    // public final void playSong(int n) {//change so gives position in seq
+    //     Song p = this.rep.entry(n);
+    //     System.out.println("Name :" + p.name());
+    //     System.out.println("Duration :" + p.duration());
+    // }
 
     /**
      * Methods.
      */
 
-    @Override
-    public final String findSong(int x) {
-        //temp
-        Sequence<Song> temp = new Sequence1L<>();
+    // @Override
+    // public final String findSong(int x) {
+    //     //temp
+    //     Sequence<Song> temp = new Sequence1L<>();
 
-        for (Map.Pair<String, Song> p : this.rep) {
-            temp.add(temp.length(), p.value());
+    //     for (Map.Pair<String, Song> p : this.rep) {
+    //         temp.add(temp.length(), p.value());
 
-        }
-        Song s = temp.entry(x);
-        return s.getName();
-    }
+    //     }
+    //     Song s = temp.entry(x);
+    //     return s.getName();
+    // }
 
-    @Override
-    public final void shuffle() {
-        assert this.rep.size() > 1 : "Must be length of greater than 1.";
+    // @Override
+    // public final void playAll() {
+    //     System.out.println("Songs: ");
+    //     for (Map.Pair<String, Song> p : this.rep) {
+    //         Song s = p.value();
 
-        Sequence<Map.Pair<String, Song>> temp = new Sequence1L<>();
+    //         System.out.println("Name : " + s.getName());
+    //     }
+    // }
 
-        for (Map.Pair<String, Song> p : this.rep) {
-            temp.add(temp.length(), p);
-        }
+    // @Override
+    // public final int duration(int x) {
+    //     //temp
+    //     Sequence<Song> temp = new Sequence1L<>();
 
-        //randomizer here here unsure of how to implement
+    //     for (Map.Pair<String, Song> p : this.rep) {
+    //         temp.add(temp.length(), p.value());
+    //     }
+    //     Song s = temp.entry(x);
 
-    }
+    //     return s.getDuration();
+    // }
 
-    @Override
-    public final void playAll() {
-        System.out.println("Songs: ");
-        for (Map.Pair<String, Song> p : this.rep) {
-            Song s = p.value();
+    // @Override
+    // public final int totalDuration() {
+    //     //temp
+    //     Sequence<Song> temp = new Sequence1L<>();
 
-            System.out.println("Name : " + s.getName());
-        }
-    }
+    //     for (Map.Pair<String, Song> p : this.rep) {
+    //         temp.add(temp.length(), p.value());
+    //     }
+    //     int result = 0;
+    //     for (Song s : temp) {
+    //         result += s.getDuration();
+    //     }
 
-    @Override
-    public final int duration(int x) {
-        //temp
-        Sequence<Song> temp = new Sequence1L<>();
-
-        for (Map.Pair<String, Song> p : this.rep) {
-            temp.add(temp.length(), p.value());
-        }
-        Song s = temp.entry(x);
-
-        return s.getDuration();
-    }
-
-    @Override
-    public final int totalDuration() {
-        //temp
-        Sequence<Song> temp = new Sequence1L<>();
-
-        for (Map.Pair<String, Song> p : this.rep) {
-            temp.add(temp.length(), p.value());
-        }
-        int result = 0;
-        for (Song s : temp) {
-            result += s.getDuration();
-        }
-
-        return result;
-    }
+    //     return result;
+    // }
 
 }
